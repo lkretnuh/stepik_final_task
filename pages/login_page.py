7058abb99e61ce70d5b3008b0dd43d0b9ca16ba3
lkretnuh <huntervok@gmail.com>
2022-04-11 17:08:56 +0300

    add login page

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Current url doesn't contains 'login'"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_form"), 'Login form is absent'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(By.CSS_SELECTOR, "#register_form"), 'Register form is absent'

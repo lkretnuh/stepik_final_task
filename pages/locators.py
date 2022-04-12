90b27bf3f878b166f4685548f6debe6f12c36b45
lkretnuh <huntervok@gmail.com>
2022-04-12 17:47:56 +0300

    add locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADDED_THE_PRODUCT =(By.CSS_SELECTOR, ".alertinner")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner>p>strong")
+    BOOKS_NAME1 = (By.CSS_SELECTOR, ".col-sm-6>h1")
+    BOOKS_NAME2 = (By.CSS_SELECTOR, ".alertinner>strong")
